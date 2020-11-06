a7bd83d5d57b3c4781ef27edc8c1736a0b66d2bb
Голофаев Никита Андреевич <NAGolofaev@sberbank.ru>
2020-11-06 14:19:14 +0300

    fix: remove inject.ts
    fixed insert css rule

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-const styleInjectPath = require
-    .resolve('style-inject-package/dist/style-inject-package.es')
-    .replace(/[\\/]+/g, '/')
+const styleInject = (css: string, packageName: string) => {
+    if (!css || typeof document === 'undefined') return;
+
+    const head = document.head || document.getElementsByTagName('head')[0];
+    let style: HTMLStyleSheetElement | null = head.querySelector(`style[${packageName}]`);
+
+    if (!style) {
+        style = document.createElement('style');
+        style.setAttribute('type', 'text/css');
+        style.setAttribute(packageName, '');
+        if (head.firstChild) {
+            head.insertBefore(style, head.firstChild);
+        } else {
+            head.appendChild(style);
+        }
+    }
+
+    if (style.styleSheet) {
+        style.styleSheet.addRule(css);
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+};
+
 
 const inject = (name: string) => (cssVariableName: string) =>
-    '\n' +
-    `import styleInject from '${styleInjectPath}';\n` +
-    `styleInject(${cssVariableName}, '${name.replace('@', '').replace('/', '-')}');`;
+    `\n (${styleInject.toString()})(${cssVariableName}, '${name.replace('@', '').replace('/', '-')}');`;
 
 export default inject;
---
+++ src/types.d.ts
@@ -0,0 +1,3 @@
+interface HTMLStyleSheetElement extends HTMLStyleElement {
+    styleSheet?: CSSStyleSheet;
+}
